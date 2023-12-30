67e62f912c086c5fba04c216bb1a9bb839d291d2
benjaminhuo <huobj@qq.com>
2023-12-30 23:45:15 +0800

    add custom data
    Signed-off-by: benjaminhuo <huobj@qq.com>

--- finetune/scripts/run_sft_lora_Yi_6b.sh
+++ finetune/scripts/run_sft_lora_Yi_6b.sh
@@ -4,15 +4,15 @@ cd ../sft/
 
 deepspeed main.py \
 	--data_path ../kse_sample_dataset/ \
-	--model_name_or_path 01-ai/Yi-6B-Chat \
-	--per_device_train_batch_size 4 \
-	--per_device_eval_batch_size 4 \
+	--model_name_or_path 01-ai/Yi-6B \
+	--per_device_train_batch_size 1 \
+	--per_device_eval_batch_size 1 \
 	--max_seq_len 4096 \
 	--learning_rate 2e-6 \
 	--weight_decay 0. \
 	--num_train_epochs 3 \
 	--training_debug_steps 20 \
-	--gradient_accumulation_steps 1 \
+	--gradient_accumulation_steps 4 \
 	--lr_scheduler_type cosine \
 	--num_warmup_steps 0 \
 	--seed 1234 \
@@ -22,4 +22,8 @@ deepspeed main.py \
 	--offload \
 	--lora_dim 128 \
 	--lora_module_name "layers." \
-	--output_dir ./output_Yi_6b_chat_sft_lora
+	--output_dir ./output_Yi_6b_sft_lora #\
+#	--output_dir ./output_Yi_6b_chat_sft_lora #\
+#	--bf16 True \
+#	--fp16 False \
+#	--torch_dtype torch.bfloat16
